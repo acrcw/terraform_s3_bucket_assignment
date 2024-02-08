7acb4f11bb064f299563905892d18857af085672
joban <joban9872@gmail.com>
2024-02-08 18:39:27 +0530

    done

--- variable.tf
+++ variable.tf
@@ -1,9 +1,10 @@
 variable "buckets_names_for_foreach" {
     type=set(string)
+    description = "List of all the BUCKET names"
     default = ["jobans-foreach-1","jobans-foreach-2","jobans-foreach-3"]
 }
 variable "buckets_names_for_count" {
     type=list(string)
-
+    description = "List of all the BUCKET names"
     default = ["jobans-count-1","jobans-count-2","jobans-count-3"]
 }
